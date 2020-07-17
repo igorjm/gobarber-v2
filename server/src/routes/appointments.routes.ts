3b1c09747055076ac6ca4dcda46674cc7c35d82c
igor.melo <igorjmelo4@gmail.com>
2020-07-16 23:06:30 -0300

    [server]: add typeorm, appointment migration

--- server/src/routes/appointments.routes.ts
+++ server/src/routes/appointments.routes.ts
@@ -1,31 +1,28 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
 // LIST ALL APPOINTMENTS
-appointmentsRouter.get('/', (req, res) => {
-  const appointments = appointmentsRepository.findAll();
+appointmentsRouter.get('/', async (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return res.json(appointments);
 });
 
 // CREATE APPOINTMENT
-appointmentsRouter.post('/', (req, res) => {
+appointmentsRouter.post('/', async (req, res) => {
   try {
     const { provider, date } = req.body;
-
     const parsedDate = parseISO(date);
+    const createAppointment = new CreateAppointmentService();
 
-    const createAppointment = new CreateAppointmentService(
-      appointmentsRepository,
-    );
-
-    const appointment = createAppointment.execute({
+    const appointment = await createAppointment.execute({
       date: parsedDate,
       provider,
     });
