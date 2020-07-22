4853c7abc2a6e66d8843ff8503798ceac1223d8d
igor.melo <igorjmelo4@gmail.com>
2020-07-21 22:12:00 -0300

    [server]: user password crypt

--- server/src/routes/appointments.routes.ts
+++ server/src/routes/appointments.routes.ts
@@ -18,13 +18,13 @@ appointmentsRouter.get('/', async (req, res) => {
 // CREATE APPOINTMENT
 appointmentsRouter.post('/', async (req, res) => {
   try {
-    const { provider, date } = req.body;
+    const { provider_id, date } = req.body;
     const parsedDate = parseISO(date);
     const createAppointment = new CreateAppointmentService();
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      provider_id,
     });
 
     return res.json(appointment);
---
+++ server/src/routes/users.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+// CREATE APPOINTMENT
+usersRouter.post('/', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return res.json(user);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+export default usersRouter;
