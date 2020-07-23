1d6cf4f271195e56f683be21211f47fa5a66f471
igor.melo <igorjmelo4@gmail.com>
2020-07-22 23:31:26 -0300

    [server]: add authenticated routes

---
+++ server/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '32816fe925a870ffaf192b53493872e7',
+    expiresIn: '1d',
+  },
+};
--- server/src/services/AuthenticateUserService.ts
+++ server/src/services/AuthenticateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface Request {
@@ -35,9 +37,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, '32816fe925a870ffaf192b53493872e7', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn: expiresIn,
     });
 
     return {
