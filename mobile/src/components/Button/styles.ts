281f6bccba493e9d35fcaebda04c81931350ddac
igor.melo <igorjmelo4@gmail.com>
2020-08-17 23:28:29 -0300

    [mobile] add components + icons

---
+++ mobile/src/components/Button/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled(RectButton)`
+  width: 100%;
+  height: 60px;
+  background: #ff9000;
+  border-radius: 10px;
+  margin-top: 8px;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+`;
